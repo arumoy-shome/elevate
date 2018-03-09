b7fdcc56e8f04023c63416a02a0bab0b291aa1e8
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 16:55:59 -0500

    Animate the coins.
    We want to rotate the coins, this can be done using the animations method
    available to all sprites.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -65,6 +65,8 @@ export default class extends Phaser.State {
         data.coins.forEach((coin) => {
             let sprite = this.coins.create(coin.x, coin.y, 'coin');
             sprite.anchor.set(0.5, 0.5);
+            sprite.animations.add('rotate', [0, 1, 2, 1], 6, true);
+            sprite.animations.play('rotate');
         });
     }
 
