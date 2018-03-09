2b7cc47e45310b303a99dd2c576c2a96d25c1e4d
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 18:13:51 -0500

    Make the spiders turn.
    Upon collision with invisible walls, we want the spiders to turn. We use
    the `touching` along with the `blocked` methods to do this. Note that we
    require to use the `blocked` method in addition to `touching` because we
    want the spiders to turn if they collide with the world bounds as well.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -159,12 +159,21 @@ class Hero extends Phaser.Sprite {
 class Spider extends Phaser.Sprite {
     constructor(game, x, y) {
         super(game, x, y, 'spider');
+        this.SPEED = 100;
         this.anchor.set(0.5, 0.5);
         this.game.physics.enable(this);
         this.body.collideWorldBounds = true;
-        this.body.velocity.x = 100;
+        this.body.velocity.x = this.SPEED;
         this.animations.add('crawl', [0, 1, 2], 8, true);
         this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
         this.animations.play('crawl');
+    }
+
+    update() {
+        if(this.body.touching.right || this.body.blocked.right) {
+            this.body.velocity.x = -this.SPEED;
+        } else if(this.body.touching.left || this.body.blocked.left) {
+            this.body.velocity.x = this.SPEED;
+        }
     }
 }
