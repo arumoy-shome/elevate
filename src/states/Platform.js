d9bad9dbbe922a6e8949a40979f2d29bbe658173
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 13:17:11 -0500

    Introduce the hero.
    Simply rendering the hero onto the canvas.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -10,17 +10,29 @@ export default class extends Phaser.State {
         this.load.image('grass4x1', 'assets/images/platform/grass_4x1.png');
         this.load.image('grass2x1', 'assets/images/platform/grass_2x1.png');
         this.load.image('grass1x1', 'assets/images/platform/grass_1x1.png');
+        this.load.image('hero', 'assets/images/platform/hero_stopped.png');
         this.load.json('level1','data/platform/level01.json');
     }
-    
+
     create() {
         this.add.image(0, 0, 'background');
         this._loadLevel(this.cache.getJSON('level1'));
     }
-    
+
     _loadLevel(data) {
         data.platforms.forEach((platform) => {
             this.add.sprite(platform.x, platform.y, platform.image);
         });
+
+        let hero = new Hero(this.game, data.hero.x, data.hero.y);
+        this.game.add.existing(hero);
+        console.log(this);
+    }
+}
+
+class Hero extends Phaser.Sprite {
+    constructor(game, x, y) {
+        super(game, x, y, 'hero');
+        this.anchor.set(0.5, 0.5);
     }
 }
