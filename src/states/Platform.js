82d51c5d82e8a1c9e5659e105d354f9ff2bb005f
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 17:25:56 -0500

    Create a custom spider sprite class.
    Spiders will have custom abilities just like our hero so we want to create
    them through a custom interface.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -126,3 +126,16 @@ class Hero extends Phaser.Sprite {
         return canJump;
     }
 }
+
+class Spider extends Phaser.Sprite {
+    constructor(game, x, y) {
+        super(game, x, y, 'spider');
+        this.anchor.set(0.5, 0.5);
+        this.game.physics.enable(this);
+        this.body.collideWorldBounds = true;
+        this.body.velocity.x = 100;
+        this.animations.add('crawl', [0, 1, 2], 8, true);
+        this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
+        this.animations.play('crawl');
+    }
+}
