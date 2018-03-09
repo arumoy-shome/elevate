1f47b1c437404e92cc84579d7d902fd47b39f3b7
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 17:05:47 -0500

    Allow hero to collect coins.
    This is another collision detection between our hero and coins. One
    difference here (vs. platforms) is that we use the `overlap` method instead
    of the `collide` since we simply want to know if the hero touched a coin
    but still allow hero to move through the coin (collide prevents sprites to
    pass through each other).

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -65,6 +65,8 @@ export default class extends Phaser.State {
         data.coins.forEach((coin) => {
             let sprite = this.coins.create(coin.x, coin.y, 'coin');
             sprite.anchor.set(0.5, 0.5);
+            this.game.physics.enable(sprite);
+            sprite.body.allowGravity = false;
             sprite.animations.add('rotate', [0, 1, 2, 1], 6, true);
             sprite.animations.play('rotate');
         });
@@ -90,6 +92,9 @@ export default class extends Phaser.State {
 
     _handleCollisions() {
         this.game.physics.arcade.collide(this.hero, this.platforms);
+        this.game.physics.arcade.overlap(this.hero, this.coins, (hero, coin) => {
+            coin.kill();
+        });
     }
 }
 
