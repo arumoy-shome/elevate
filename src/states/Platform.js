e699de62d652b5ef2f0dc301fb406ed4d62d356e
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 15:42:53 -0500

    Move hero using physics engine.
    Previously we were moving the hero by setting it's position on the canvas
    directly however a better way to implement this would be to use physics.
    Phaser has a built in, physics engine called Phaser Arcade which allows us
    to use speed and velocity to implement movement.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -45,6 +45,8 @@ export default class extends Phaser.State {
             this.hero.move(-1);
         } else if(this.keys.right.isDown) {
             this.hero.move(1);
+        } else {
+            this.hero.move(0);
         }
     }
 }
@@ -53,9 +55,11 @@ class Hero extends Phaser.Sprite {
     constructor(game, x, y) {
         super(game, x, y, 'hero');
         this.anchor.set(0.5, 0.5);
+        this.game.physics.enable(this);
     }
 
     move(direction) {
-        this.x += direction * 2.5
+        const SPEED = 200;
+        this.body.velocity.x = direction * SPEED;
     }
 }
