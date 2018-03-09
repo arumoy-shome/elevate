88d50698a84dfe53887f06cc568eaba86e08c352
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-09 16:26:39 -0500

    Load jump sound.
    This commit also does a quick refactor to the way our assets are loaded.
    Instead of using the `load` method on the global object, we use it's
    reference through `this.game`. This is better than dealing with the global
    object directly.

--- src/states/Platform.js
+++ src/states/Platform.js
@@ -11,15 +11,16 @@ export default class extends Phaser.State {
     }
 
     preload() {
-        this.load.image('background', 'assets/images/platform/background.png');
-        this.load.image('ground', 'assets/images/platform/ground.png');
-        this.load.image('grass8x1', 'assets/images/platform/grass_8x1.png');
-        this.load.image('grass6x1', 'assets/images/platform/grass_6x1.png');
-        this.load.image('grass4x1', 'assets/images/platform/grass_4x1.png');
-        this.load.image('grass2x1', 'assets/images/platform/grass_2x1.png');
-        this.load.image('grass1x1', 'assets/images/platform/grass_1x1.png');
-        this.load.image('hero', 'assets/images/platform/hero_stopped.png');
-        this.load.json('level1','data/platform/level01.json');
+        this.game.load.image('background', 'assets/images/platform/background.png');
+        this.game.load.image('ground', 'assets/images/platform/ground.png');
+        this.game.load.image('grass8x1', 'assets/images/platform/grass_8x1.png');
+        this.game.load.image('grass6x1', 'assets/images/platform/grass_6x1.png');
+        this.game.load.image('grass4x1', 'assets/images/platform/grass_4x1.png');
+        this.game.load.image('grass2x1', 'assets/images/platform/grass_2x1.png');
+        this.game.load.image('grass1x1', 'assets/images/platform/grass_1x1.png');
+        this.game.load.image('hero', 'assets/images/platform/hero_stopped.png');
+        this.game.load.audio('sfxJump', 'assets/sounds/platform/jump.wav');
+        this.game.load.json('level1','data/platform/level01.json');
     }
 
     create() {
