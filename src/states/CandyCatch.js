19a2f3029a35ce2d248d289a7f8cca8774757cd9
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-10 13:10:55 -0500

    Let player move.
    We want our hero to be able to go left and right.

--- src/states/CandyCatch.js
+++ src/states/CandyCatch.js
@@ -3,6 +3,10 @@ import config from '../config';
 
 export default class extends Phaser.State {
     init() {
+        this.keys = this.input.keyboard.addKeys({
+            left: Phaser.KeyCode.LEFT,
+            right: Phaser.KeyCode.RIGHT
+        });
         // this.player = null;
         // this._candyGroup = null;
         // this._spawnCandyTimer = 0;
@@ -11,7 +15,7 @@ export default class extends Phaser.State {
         // this._scoreText = null;
         // this._health = 0;
     }
-    
+
     preload() {
         this.game.load.image('background', 'assets/images/platform/background.png');
         this.game.load.image('ground', 'assets/images/platform/ground.png');
@@ -25,20 +29,7 @@ export default class extends Phaser.State {
     create () {
         this.game.add.image(0, 0, 'background');
         this._loadLevel(this.game.cache.getJSON('level-catch'));
-        // this.physics.startSystem(Phaser.Physics.ARCADE);
-        // this.physics.arcade.gravity.y = 100;
-        
-        // this.game.add.image(0, 0, 'background');
-        // this.add.sprite(-30, config.candyCatch.height-160, 'floor');
-        // this.add.sprite(10, 5, 'score-bg');
-        
-        // this.player = this.add.sprite(5, 760, 'monster-idle');
-        // this.player.animations.add('idle',
-        //                             [0,1,2,3,4,5,6,7,8,9,10,11,12],
-        //                             10,
-        //                             true);
-        // this.player.animations.play('idle');
-        
+
         // this._fontStyle = { font: "40px Arial",
         //                     fill: "#FFCC00",
         //                     stroke: "#333",
@@ -57,10 +48,10 @@ export default class extends Phaser.State {
         this._spawnPlatforms(data);
         this._spawnHero(data);
     }
-    
+
     _spawnPlatforms(data) {
         this.platforms = this.game.add.group();
-        
+
         data.platforms.forEach((platform) => {
             let sprite = this.platforms.create(platform.x, platform.y, platform.image);
             this.game.physics.enable(sprite);
@@ -75,16 +66,17 @@ export default class extends Phaser.State {
     }
 
     update() {
+        this._handleInput();
         // this._spawnCandyTimer += this.time.elapsed;
         // if(this._spawnCandyTimer > 1000) {
         //     this._spawnCandyTimer = 0;
         //     this.spawnCandy();
         // }
-        
+
         // this._candyGroup.forEach((candy) => {
         //     candy.angle += candy.rotateMe;
         // });
-        
+
         // if(!this._health) {
         //     this.add.sprite((config.candyCatch.width-594)/2,
         //                     (config.candyCatch.height-271)/2,
@@ -94,17 +86,27 @@ export default class extends Phaser.State {
         // }
     }
 
+    _handleInput() {
+        if(this.keys.left.isDown) {
+            this.hero.move(-1);
+        } else if(this.keys.right.isDown) {
+            this.hero.move(1);
+        } else {
+            this.hero.move(0);
+        }
+    }
+
     spawnCandy() {
         // let dropPos = Math.floor(Math.random()*config.candyCatch.width);
         // let dropOffset = [-27,-36,-36,-38,-48];
         // let candyType = Math.floor(Math.random()*5);
         // let candy = this.add.sprite(dropPos, dropOffset[candyType], 'candy');
-        
+
         // candy.animations.add('anim', [candyType], 10, true);
         // candy.animations.play('anim');
-        
+
         // this.physics.enable(candy, Phaser.Physics.ARCADE);
-        
+
         // candy.inputEnabled = true;
         // candy.events.onInputDown.add(() => {
         //     candy.kill();
@@ -135,5 +137,16 @@ class Hero extends Phaser.Sprite {
         // this.animations.add('run', [1, 2], 8, true);
         // this.animations.add('jump', [3]);
         // this.animations.add('fall', [4]);
+    }
+
+    move(direction) {
+        const SPEED = 200;
+        this.body.velocity.x = direction * SPEED;
+
+        if(this.body.velocity.x < 0) {
+            this.scale.x = -1;
+        } else if(this.body.velocity.x > 0) {
+            this.scale.x = 1;
+        }
     }
 }
