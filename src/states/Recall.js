2bcf285de85a82b56fca82af83d13f275b749882
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-16 19:39:45 -0400

    Preload the assets for the game.

--- src/states/Recall.js
+++ src/states/Recall.js
@@ -5,4 +5,9 @@ export default class extends Phaser.State {
     init(data) {
         this.simonSequence = data.simonSequence || [];
     }
+    
+    preload() {
+        this.game.load.json('simon','data/simon/simon.json');
+        this.game.load.spritesheet('item', 'assets/images/simon/grocery-list.jpg', 160, 160);
+    }
 }
