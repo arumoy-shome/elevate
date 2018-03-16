4aaf05fe08eb4b68a38febfa447e271948f9a95b
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-16 19:41:37 -0400

    Set background and load level details.

--- src/states/Recall.js
+++ src/states/Recall.js
@@ -5,9 +5,14 @@ export default class extends Phaser.State {
     init(data) {
         this.simonSequence = data.simonSequence || [];
     }
-    
+
     preload() {
         this.game.load.json('simon','data/simon/simon.json');
         this.game.load.spritesheet('item', 'assets/images/simon/grocery-list.jpg', 160, 160);
+    }
+
+    create() {
+        this.game.stage.backgroundColor = "#f2f2f2";
+        this.data = this.game.cache.getJSON('simon');
     }
 }
