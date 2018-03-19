10abf417ff78ad1b423c2a8258c5f19601a2f0df
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-19 03:09:01 -0400

    Make the leaderboard pretty

--- src/states/LeaderBoard.js
+++ src/states/LeaderBoard.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import Question from '../sprites/Question';
 
 export default class extends Phaser.State {
     init(data) {
@@ -6,17 +7,17 @@ export default class extends Phaser.State {
         this.candyCatchScore = data.metrics.candyCatch.finalScore;
         this.recallScore = data.metrics.recall.score;
     }
-    
+
     create() {
-        this.game.add.image(0, 0, 'background');
-        let style = { font: "40px Arial",
-                              fill: "#FFCC00",
-                              stroke: "#333",
-                              strokeThickness: 5,
-                              align: "center" };
-        this.game.add.text(400, 100, `Registration: ${this.simonScore}`, style);
-        this.game.add.text(400, 150, `Attention & Calculation: ${this.candyCatchScore}`, style);
-        this.game.add.text(400, 200, `Recall: ${this.recallScore}`, style);
+        this.game.stage.backgroundColor = "#f2f2f2";
+        let scores = this.game.add.group();
+        let registrationText = new Question(this.game, this.game.world.centerY-50, `Registration: ${this.simonScore} / 3`)
+        let calculationText = new Question(this.game, this.game.world.centerY, `Attention & Calculation: ${this.candyCatchScore} / 5`)
+        let recallText = new Question(this.game, this.game.world.centerY+50, `Recall: ${this.recallScore} / 3`)
+
+        this.game.add.existing(registrationText);
+        this.game.add.existing(calculationText);
+        this.game.add.existing(recallText);
         this.game.paused = true;
     }
 }
