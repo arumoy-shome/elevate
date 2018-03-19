6ad02fb3d7f11f2b23e15806b99f4fcfdd6ddca5
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-18 21:09:35 -0400

    Refactor: position of start button
    It's better to center the anchor rather than doing weird pixel math!

--- src/sprites/StartButton.js
+++ src/sprites/StartButton.js
@@ -1,16 +1,15 @@
 import Phaser from 'phaser';
 import config from '../config';
 
-const BTN_WIDTH = 401;
-const BTN_HEIGHT = 143;
-
 export default class extends Phaser.Button {
     constructor(game, callback, context) {
         super(game,
-              (config.width - BTN_WIDTH)/2,
-              (config.height - BTN_HEIGHT)/2,
+              game.world.centerX,
+              game.world.centerY,
               'button-start',
               callback,
               context);
+
+        this.anchor.setTo(0.5);
     }
 }
